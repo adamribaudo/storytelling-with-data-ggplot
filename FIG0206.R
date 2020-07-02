119b764d8e3dafd38a53e85418b39c2d1e3ea7e6
Wal McConnell <walter.mcconnell@gmail.com>
2020-07-02 22:48:45 +0100

    start theme_swd

--- FIG0206.R
+++ FIG0206.R
@@ -1,27 +1,21 @@
+rm(list=ls())
 library(tidyverse)
-source("include.R")
-theme_set(theme_minimal() + theme(panel.grid.major = element_blank(),
-                                  panel.grid.minor = element_blank(),
-                                  axis.line = element_line(size = .1, color=GRAY9),
-                                  axis.text = element_text(color = GRAY7, size = 10),
-                                  axis.title = element_text(color = GRAY3, size = 10),
-                                  axis.title.y = element_text(hjust = 1, margin = margin(0,6,0,15,"pt")),
-                                  axis.title.x = element_text(hjust = 0, margin = margin(6,0,15,0,"pt")),
-                                  plot.title = element_text(hjust = -.65)))
+source("theme/theme_swd.R")
 
-df <- read_csv("data\\FIG0206-7.csv") %>% 
+df <- read_csv(file.path("data", "FIG0206-7.csv")) %>% 
   mutate(`Cost Per Mile` = as.numeric(str_remove_all(`Cost Per Mile`,"\\$"))) 
 
 avg_miles <- mean(df$`Miles Driven`)
 avg_cost <- mean(df$`Cost Per Mile`)
 
-pt <- ggplot(df) + geom_point(aes(x=`Miles Driven`,y=`Cost Per Mile`), color = GRAY9, size = 2.5) +
-  scale_y_continuous(breaks = seq(0,3,.5), limits = c(0,3),labels = scales::dollar_format(prefix="$")) + 
-  scale_x_continuous(limits = c(0,4000), label = scales::comma) + 
-  geom_point(x = avg_miles,y=avg_cost,size=3.5) + 
-  geom_label(aes(x=avg_miles, y=avg_cost), label = "AVG", nudge_x = -300, nudge_y = .01, size = 3.5, label.size = 0, label.padding = unit(.17, "lines")) + 
-  labs(title = "Cost per mile by miles driven", x = "Miles driven per month", y = "Cost per mile") 
+pt <- ggplot(df) + 
+  geom_point(aes(x = `Miles Driven`,y = `Cost Per Mile`), color = GRAY9, size = 2.5) +
+  scale_y_continuous(breaks = seq(0,3.5,.5), limits = c(0,3), labels = scales::dollar_format()) + 
+  scale_x_continuous(limits = c(0,4000), label = scales::comma) +
+  geom_point(x = avg_miles, y = avg_cost, size=3.5) + 
+  geom_text(x = avg_miles, y = avg_cost, label = "AVG", hjust = 1.25) +  
+  labs(title = "Cost per mile by miles driven", x = "Miles driven per month", y = "Cost per mile") +
+  theme_swd()
 
-ggsave("plot output\\FIG0206.png", pt, width = 4.5, height = 4.2)
+ggsave(file.path("plot output", "FIG0206.png"), pt)
 pt
-  
--- FIG0207.R
+++ FIG0207.R
@@ -1,30 +1,23 @@
 library(tidyverse)
-source("include.R")
-theme_set(theme_minimal() + theme(panel.grid.major = element_blank(),
-                                  panel.grid.minor = element_blank(),
-                                  axis.line = element_line(size = .1, color=GRAY9),
-                                  axis.text = element_text(color = GRAY7, size = 10),
-                                  axis.title = element_text(color = GRAY3, size = 10),
-                                  axis.title.y = element_text(hjust = 1, margin = margin(0,6,0,15,"pt")),
-                                  axis.title.x = element_text(hjust = 0, margin = margin(6,0,15,0,"pt")),
-                                  plot.title = element_text(hjust = -.65)))
+source("theme/theme_swd.R")
 
-df <- read_csv("data\\FIG0206-7.csv") %>% 
-  mutate(`Cost Per Mile` = as.numeric(str_remove_all(`Cost Per Mile`,"\\$"))) %>%
-  mutate(color = if_else(`Cost Per Mile` < mean(`Cost Per Mile`), GRAY9,ORANGE1))
+df <- read_csv(file.path("data", "FIG0206-7.csv")) %>% 
+  mutate(`Cost Per Mile` = parse_number(`Cost Per Mile`),
+         avg_miles = mean(`Cost Per Mile`, na.rm = TRUE),
+         avg_cost = mean(`Cost Per Mile`, na.rm = TRUE),
+         color = if_else(`Cost Per Mile` < avg_miles, GRAY9, ORANGE1))
 
-avg_miles <- mean(df$`Miles Driven`)
-avg_cost <- mean(df$`Cost Per Mile`)
-
-pt <- ggplot(df) + geom_point(aes(x=`Miles Driven`,y=`Cost Per Mile`, color = color), size = 2.5) +
+pt <- ggplot(df) + 
+  geom_point(aes(x=`Miles Driven`,y=`Cost Per Mile`, color = color), size = 3) +
   scale_color_identity() + 
-  scale_y_continuous(breaks = seq(0,3,.5), limits = c(0,3),labels = scales::dollar_format(prefix="$")) + 
+  scale_y_continuous(breaks = seq(0,3,.5), limits = c(0,3),labels = scales::dollar_format()) + 
   scale_x_continuous(limits = c(0,4000), label = scales::comma) + 
   geom_hline(yintercept = avg_cost, linetype = "longdash") + 
   geom_point(x = avg_miles,y=avg_cost,size=3.5) + 
-  geom_label(aes(x=avg_miles, y=avg_cost), label = "AVG", nudge_x = -300, nudge_y = .01, size = 3.5, label.size = 0, label.padding = unit(.17, "lines")) + 
-  labs(title = "Cost per mile by miles driven", x = "Miles driven per month", y = "Cost per mile") 
+  geom_label(x = avg_miles, y = avg_cost, label = "AVG", hjust = 1.25, label.size = 0) +  
+  labs(title = "Cost per mile by miles driven", x = "Miles driven per month", y = "Cost per mile") +
+  theme_swd()
 
-ggsave("plot output\\FIG0207.png", pt, width = 4.5, height = 4.2)
+ggsave(file.path("plot output", "FIG0207.png"), pt, width = 4.5, height = 4.2)
 pt
   
