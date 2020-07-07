8e332a6f85f751562dc4aad76d7fab1f9ada2583
Wal McConnell <walter.mcconnell@gmail.com>
2020-07-07 20:14:51 +0100

    flip to use theme_swd and ggtext/geom_richtext

--- FIG0220.R
+++ FIG0220.R
@@ -4,17 +4,16 @@
 
 
 library(tidyverse)
-source("include.R")
+library(ggtext)
+source("theme/theme_swd.R")
 
-theme_set(theme_minimal() + theme(panel.grid.major = element_blank(),
-                                  panel.grid.minor = element_blank(),
-                                  axis.title = element_blank(),
-                                  axis.ticks = element_blank(),
-                                  axis.line = element_blank(),
-                                  axis.text = element_blank(),
-                                  plot.title = element_text(hjust=.1),
-                                  plot.margin = unit(c(1,4,1,1),"cm"),
-                                  ))
+theme_set(theme_swd() + theme(axis.title.x = element_blank(),
+                              axis.title.y = element_blank(),
+                              axis.ticks.x = element_blank(),
+                              axis.ticks.y = element_blank(),
+                              axis.line = element_blank(),
+                              axis.text = element_blank(),
+                              plot.margin = unit(c(1,4,1,1),"cm")))
 
 df_phone <- expand.grid(x = 0:9,y=0:9)
 df_interview <- expand.grid(x = 5:9,y=0:4)
@@ -25,11 +24,9 @@ pt <- ggplot() +
   geom_tile(aes(x,y),data=df_interview,color = "white", fill = GRAY6) + 
   geom_tile(aes(x,y),data=df_offer,color = "white", fill = GREEN3) + 
   labs(title = "Interview Breakdown") + coord_cartesian(clip = "off") +
-  annotate("text", label = "Out of every 100\nphone screens...", x = 10, y = 8, color= GRAY9, hjust = 0) + 
-  annotate("text", label = "we bring 25\ncandidates\nonsite for\ninterviews...", x = 10, y = 3.5, color= GRAY6, hjust = 0) +
-  annotate("text", label = "and", parse = F, x = 10, y = 0, color= GREEN3, hjust = 0) +
-  
-  annotate("text", label = "bold('extend 9 offers.')", parse = T, x = 10, y = -.6, color= GREEN3, hjust = 0) 
+  geom_richtext(aes(x = 10, y = 8), label = "Out of every **100<br/>phone screens**...",color= GRAY9, hjust = 0, label.color = NA) +
+  geom_richtext(aes(x = 10, y = 3.5), label = "we bring **25 <br/>candidates onsite** <br/>for\ninterviews...",color= GRAY6, hjust = 0, label.color = NA) +  
+  geom_richtext(aes(x = 10, y = 0), label = "and <br /> **extend 9 offers.**",color= GREEN3, hjust = 0, label.color = NA) +    
 
-ggsave("plot output\\FIG0220.png", pt, width = 5, height = 4)
+ggsave(file.path("plot output", "FIG0220.png"), pt, width = 5, height = 4)
 pt
