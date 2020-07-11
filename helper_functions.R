ac677ce97a7162c5166cd54406cb399462744ec4
Adam Ribaudo <adam.ribaudo@gmail.com>
2020-07-11 09:30:23 -0400

    Commenting out dev.new() call to save some hassle

--- helper_functions.R
+++ helper_functions.R
@@ -1,7 +1,8 @@
 # Function to both save a ggplot to file but also render it to the screen in the specified
 # height and width
 save_and_show_plot <- function(plot, width, height, file_name) {
-  dev.new(width = width, height = height, noRStudioGD = T)
+  # Uncomment below to display the plot in a new window with the specified width/height which should match the ggsave output
+  # dev.new(width = width, height = height, noRStudioGD = T)
   print(plot)
   ggsave(filename = file.path("plot output", file_name), plot = plot, width = width, height = height)
 }
